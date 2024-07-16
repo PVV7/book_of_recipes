5aa423c414229c44e3cdad4a2278176991010d96
Plotnikov <plotnikovveniamin@gmail.com>
2024-07-16 21:03:53 +0200

    add: reset password unfinished

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use \App\Http\Controllers\Auth\RegisterController;
-use \App\Http\Controllers\Auth\DashboardController;
+use App\Http\Controllers\Auth\DashboardController;
+use App\Http\Controllers\Auth\ForgotPasswordController;
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\ResetPasswordController;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -27,7 +29,12 @@ Route::post('/register', [RegisterController::class, 'store'])->middleware('gues
 Route::get('/login', [LoginController::class, 'actionIndex'])->name('login');
 Route::post('/login', [LoginController::class, 'store']);
 
-//Route::post('/logout', [LoginController::class, 'destroy'])->middleware('auth')->name('logout');
-Route::get('/logout', [LoginController::class, 'destroy'])->middleware('auth')->name('logout');
+Route::post('/logout', [LoginController::class, 'destroy'])->middleware('auth')->name('logout');
 
 Route::get('/dashboard', [DashboardController::class, 'actionIndex'])->middleware('auth')->name('dashboard');
+
+Route::get('/forgot-password', [ForgotPasswordController::class, 'actionIndex'])->middleware('guest')->name('password.request');
+Route::post('/forgot-password', [ForgotPasswordController::class, 'store'])->middleware(['guest','throttle:3,1'])->name('password.email');
+
+Route::get('/reset-password/{token}', [ResetPasswordController::class, 'actionIndex'])->middleware('guest')->name('password.reset');
+Route::post('/reset-password', [ResetPasswordController::class, 'store'])->middleware('guest')->name('password.update');
